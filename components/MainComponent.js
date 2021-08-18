1467b5dbbf67425b06e2c0d491680f86213d3061
Baiba Apkalna <apkalna@gmail.com>
2021-08-19 02:03:48 +0300

    Navigation Colors

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -3,11 +3,13 @@ import Home from './HomeComponent';
 import Blog from './BlogComponent';
 import AnimalBlog from './AnimalsBlogComponent';
 import Constants from 'expo-constants';
-import { View, Platform, StyleSheet } from 'react-native';
+import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
-import { createDrawerNavigator } from 'react-navigation-drawer';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 import { Icon } from 'react-native-elements';
+import SafeAreaView from 'react-native-safe-area-view';
+
 
 const BlogNavigator = createStackNavigator(
     {
@@ -61,17 +63,68 @@ const HomeNavigator = createStackNavigator(
     }
 );
 
-
+const CustomDrawerContentComponent = props => (
+    <ScrollView>
+        <SafeAreaView 
+            style={styles.container}
+            forceInset={{top: 'always', horizontal: 'never'}}>
+            <View style={styles.drawerHeader}>
+                <View style={{flex: 1}}>
+                    <Image source={require('./images/logo.jpg')} style={styles.drawerImage} />
+                </View>
+                <View style={{flex: 2}}>
+                    <Text style={styles.drawerHeaderText}>City Farm</Text>
+                </View>
+            </View>
+            <DrawerItems {...props} />
+        </SafeAreaView>
+    </ScrollView>
+);
 
 const MainNavigator = createDrawerNavigator(
     {
-        Home: { screen: HomeNavigator },
-        Blog: { screen: BlogNavigator },
-       
+        Home: {
+            screen: HomeNavigator,
+            navigationOptions: {
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='home'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        Blog: {
+            screen: BlogNavigator,
+            navigationOptions: {
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='list'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        
     },
     {
-        drawerBackgroundColor: '#234A12'
+        drawerBackgroundColor: '#234A12',
+        contentComponent: CustomDrawerContentComponent,
+        contentOptions: {
+            activeTintColor: '#D5E9D7',
+            itemsContainerStyle: {
+              marginVertical: 0,
+            },
+            iconContainerStyle: {
+              opacity: 1
+            }
+          }
     }
+    
 );
 
 const AppNavigator = createAppContainer(MainNavigator)
@@ -89,7 +142,31 @@ class Main extends Component {
     }
 }
 
+
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        color: '#D5E9D7'
+    },
+    drawerHeader: {
+        backgroundColor: '#89A98C',
+        height: 100,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: '#234A12',
+        fontSize: 24,
+        fontWeight: 'bold'
+    },
+    drawerImage: {
+        margin: 10,
+        height: 60,
+        width: 60,
+        borderRadius: 50,
+    },
     stackIcon: {
         marginLeft: 10,
         color: '#D5E9D7',
